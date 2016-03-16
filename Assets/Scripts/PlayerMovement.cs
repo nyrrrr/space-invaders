79f8ecd6ec03d01d2c7fb76dd7d28eb822882d1e
nyrrrr <xnamespooferx@gmail.com>
2016-03-16 10:56:46 +0100

    added ship w/ horizontal movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Transform go;
+
+    Rigidbody2D rigidBody;
+
+    float fHorizontal, fSpeed = 0.2f;
+
+	// Use this for initialization
+	void Awake () {
+        go = this.transform;
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fHorizontal = Input.GetAxisRaw("Horizontal");
+        go.Translate(fHorizontal * fSpeed, 0, go.position.z);
+	}
+}
