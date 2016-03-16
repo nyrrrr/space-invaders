4a962b641ab973e66dff6beb17573215b35fd0f8
nyrrrr <xnamespooferx@gmail.com>
2016-03-16 15:36:12 +0100

    added basic shooting and went thru OnTriggerHell

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-    Transform go;
+    public Transform gun;
 
     Rigidbody2D rigidBody;
 
-    float fHorizontal, fSpeed = 10f;
+    public GameObject projectilePrefab;
+    GameObject projectile = null;
 
-	// Use this for initialization
-	void Awake () {
-        go = this.transform;
+    float fHorizontal, fSpeed = 10f, fShoot;
+
+    // Use this for initialization
+    void Awake()
+    {
         rigidBody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        fShoot = Input.GetAxisRaw("Jump");
+        if (fShoot > 0)
+        {
+            projectile = (GameObject)Instantiate(projectilePrefab, gun.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per fixed frame rate
+    void FixedUpdate()
+    {
         fHorizontal = Input.GetAxisRaw("Horizontal");
         rigidBody.velocity = Vector2.right * fHorizontal * fSpeed;
-	}
+    }
 }
---
+++ Assets/Scripts/ShotBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior : MonoBehaviour
+{
+
+    float fSpeed = 4f;
+    Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.velocity = Vector2.up * fSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag != "Player") Destroy(gameObject);
+    }
+}
